1b578b81ee6489321c0028060f68fc3c945125b9
jimao <shihao.gu@perfma.com>
2024-11-11 13:23:31 +0800

    提交

--- src/nft.service.ts
+++ src/nft.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
+const list = [
+  { id: '1', name: 'jimao' },
+  { id: '2', name: 'btc' },
+  { id: '3', name: 'eth' },
+];
+
 @Injectable()
 export class NftService {
   public list(): any[] {
-    return [
-      { id: 1, name: 'jimao' },
-      { id: 2, name: 'btc' },
-      { id: 3, name: 'eth' },
-    ];
+    return list;
   }
 
   public detail(id: string) {
-    return { id, name: 'btc' };
+    return list.find((item) => item.id === id);
   }
 }
